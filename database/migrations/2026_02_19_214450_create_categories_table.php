c314e1248a0d05e87d4f20833d72aba95bd96075
mdsafaet <mdsafaet84@gmail.com>
2026-02-24 05:41:51 +0600

    fixed the categories maigration

--- database/migrations/2026_02_19_214450_create_categories_table.php
+++ database/migrations/2026_02_19_214450_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('categoryName', 50);
-            $table->string('categoryImg', 300)->nullable();
+            $table->string('categoryImg', 300);
 
             $table->timestamps();
         });
